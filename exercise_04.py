1b47d0467209b8502b94e94e186da9fbaa5d188d
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2021-06-10 14:12:56 +0100

    Create exercise_04.py

---
+++ exercise_04.py
@@ -0,0 +1,16 @@
+"""Exercise 4
+Create a program that asks the user for a number and then prints out a list of all the divisors of that number. 
+"""
+
+
+num = int(input"Please choose a number to divide: "))
+
+listRange = list(range(1,num+1))
+
+divisorList = [)
+
+for number in Range:
+   if num % number = 0:
+      divisor_List.append(number)
+
+print(divisorList)

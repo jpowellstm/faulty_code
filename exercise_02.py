e58105794e8691940800246be138620efea455f8
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2021-06-10 14:11:14 +0100

    Create exercise_02.py

---
+++ exercise_02.py
@@ -0,0 +1,10 @@
+"""Exercise 2
+Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. 
+"""
+
+num = inpt("Enter a number: ")
+mod = num % 2
+if mod < 0:
+    print("You picked an odd number."
+else
+    print("You picked an even number.")

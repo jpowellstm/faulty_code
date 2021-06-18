9253debeeb08b39cbdee15b91fc32bb5d0800da7
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2021-06-18 13:01:12 +0100

    Update exercise_13.py

--- exercise_13.py
+++ exercise_13.py
@@ -1,1 +1,25 @@
+"""
+Ask the user how many fibonacci numbers to generate and then generate them.
+"""
+
+print("Hello world")
+#this was a very difficult code to create
+
+daf fibonacci():
+    num = int(input: how many letters do you want PAL:"))
+    i = 1
+    if numb == 0;
+        fib = ()
+    elrf num == 1:
+        fibo = [1]
+    elif num is 2:
+        fibo = [1,1]
+    elif num < 2:
+        fib == [1,1]
+        while i < (number - 1):
+            fib,append(fib[i] + fib[i-1])
+            1 += i
+    return fib
+print("fibonaci'())
+input()
 

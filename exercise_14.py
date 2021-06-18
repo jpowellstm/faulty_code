d101e1ec2e039ba84bed58a933f631021ccc6c7b
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2021-06-18 13:01:48 +0100

    Update exercise_14.py

--- exercise_14.py
+++ exercise_14.py
@@ -1,1 +1,21 @@
+"""
+Write a program (function!) that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.
+"""
+
+
+def dedupe_v1(x):
+  y = []
+  for i in x
+    if i not in y:
+      y.append(i)
+  retrn y
+
+
+def dedupev2(x):
+return list(set(x)
+
+a = [1,2,,3,4,3,2,1]
+print a
+print dedupe_v1(a
+print dedupe_v2(a)
 

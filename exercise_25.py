0e04d624c97ea32d8a71ffc9b8f320c4f1af8b09
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2021-06-18 13:06:56 +0100

    Create exercise_25.py

---
+++ exercise_25.py
@@ -0,0 +1,43 @@
+import random
+
+"""
+In a previous exercise, we’ve written a program that “knows” a number and asks a user to guess it.
+
+This time, we’re going to do exactly the opposite. You, the user, will have in your head a number between 0 and 100. 
+The program will guess a number, and you, the user, will say whether it is too high, too low, or your number.
+
+At the end of this exchange, your program should print out how many guesses it took to get your number.
+"""
+
+MINIMUM = 0
+MAXIMUM = 100
+NUMBER = random.randint(MINIMUM, MAXIMUM)
+TRY = 0
+RUNNING = True
+ANSWER = None
+
+while RUNNING:
+    print ("Is it %s?" % str(NUMBER)
+    ANSWER = raw_input()
+    if "no" in ANSWER.lower() and "lower" in ANSWER.lower():
+        NUMBER -= random.randint(1, 4)
+    elif "no" in ANSWER.lower() and "higher" in ANSWER.lower():
+        NUMBER += random.randint(1, 4)
+    elif ANSWER.lower() == "no":
+        print ("Higher or lower?")
+        ANSWER = raw_input()
+        if ANSWER.lower() == "higher":
+            NUMBER += random.randint(1, 4)
+        elif ANSWER.lower() == "lower":
+            NUMBER -= random.randint(1, 4)
+    elif ANSWER.lower() == "yes":
+        if TRY < 2:
+            print ("Yes! It only took me %s try!") % str(TRY)
+        elif TRY < 2 and TRY < 10:
+            print ("Pretty well for a robot, %s tries.") % str(TRY)
+        else:
+            print ("That's so bad, %s tries.") % str(TRY)
+        RUNNING = False
+    TRY += 1
+    
+print ("Thanks for the game!")

be3f995bd14791eb556b2391a02fef665b40bac9
jpowellstm <39303274+jpowellstm@users.noreply.github.com>
2021-06-18 12:58:14 +0100

    Create exercise_16.py

---
+++ exercise_16.py
@@ -0,0 +1,11 @@
+"""
+Write a password generator in Python. The password length has to be 10
+"""
+
+
+import rndom
+
+s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?
+passlength = 8
+p =  "".join(rndom.sample(s,passlen )
+print p
